61c47b2b7d0c9fdded84ed0bd6b3dab9bafdef78
Heorhii Pashniev <heorhii_pashniev@epam.com>
2024-03-25 10:23:35 +0200

    Add actuator

---
+++ IdeaProjects/Jym/src/main/java/com/example/jymapplication/actuator/MemoryHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.example.jymapplication.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemoryHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        double freeMemoryPercentage = ((double) freeMemory / totalMemory) * 100;
+
+        if (freeMemoryPercentage > 10) {
+            return Health.up().withDetail("OK","A lot of memory enable").build();
+        } else {
+            return Health.down().withDetail("Error", "Low memory available").build();
+        }
+
+    }
+}
