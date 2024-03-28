a873dc3c34aa0ca8aa7119474f39c835ac194f34
Heorhii Pashniev <heorhii_pashniev@epam.com>
2024-03-28 13:08:25 +0200

    security

---
+++ IdeaProjects/Jym/src/main/java/com/example/jymapplication/response/UserResponse.java
@@ -0,0 +1,11 @@
+package com.example.jymapplication.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponse {
+    String username;
+    String token;
+}
---
+++ IdeaProjects/Jym/src/main/java/com/example/jymapplication/utils/PasswordHashing.java
@@ -0,0 +1,21 @@
+package com.example.jymapplication.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+@Component
+public class PasswordHashing {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedPassword = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
